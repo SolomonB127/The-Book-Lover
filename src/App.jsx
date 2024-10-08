9732afb5bbd6cf505f3f05ba71cb8b3fc8eae87e
SolomonB127 <solomonakinsulire@gmail.com>
2024-10-08 17:51:39 +0100

    Added the NavBar components into file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
+import Navbar from "./Components/Navbar";
+
 import "./App.css";
 function App() {
   return (
     <>
-      <div className="flex min-h-screen items-center justify-center bg-gray-100">
-        <h1 className="text-3xl font-bold text-blue-600">
-          Welcome to Tailwind + React!
-        </h1>
-      </div>
+      <Navbar />
     </>
   );
 }
