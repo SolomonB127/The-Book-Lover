1df19d25a486f7900bb6af4be23d51c86d6447ed
SolomonB127 <solomonakinsulire@gmail.com>
2024-10-10 01:07:29 +0100

    feat:Modified to be Responsive

--- src/Components/HomeComponents/Hero.jsx
+++ src/Components/HomeComponents/Hero.jsx
@@ -3,26 +3,30 @@ import TypewriterComponent from "typewriter-effect";
 
 const Hero = () => {
     return (
-        <section className='mx-auto flex min-h-screen w-full max-w-7xl items-center justify-start bg-[url("/src/assets/T.B.L_Cover.jpg")] bg-cover bg-center bg-no-repeat p-4 font-black text-black'>
-            <div className="text-left text-9xl sm:text-4xl md:text-5xl">
-                A Cozy Evening, <br />
-                A Perfect Escape, <br />
-                Your Quiet Space, <br /> <br />
-                <TypewriterComponent
-                onInit={(typewriter) => {
-                    typewriter
-                    .typeString("Welcome to Your Sanctuary!")
-                    .pauseFor(1500)
-                    .start();
-                }}
-                options={{
-                    autoStart: true,
-                    loop: false,
-                    delay: 75,
-                    deleteSpeed: 50,
-                    cursor: "|",
-                }}
-                />
+        <section className='flex items-center justify-start min-h-screen w-full p-4 mx-auto bg-[url("/src/assets/T.B.L_Cover.jpg")] bg-cover bg-center bg-no-repeat font-black text-black max-w-7xl'>
+            <div className="text-center sm:text-left">
+                <div className="text-5xl sm:text-6xl md:text-7xl lg:text-6xl">
+                    A Cozy Evening, <br />
+                    A Perfect Escape, <br />
+                    Your Quiet Space, <br /> <br />
+                </div>
+                <div className="text-1xl sm:text-6xl">
+                    <TypewriterComponent
+                        onInit={(typewriter) => {
+                            typewriter
+                                .typeString("Welcome to Your Sanctuary!")
+                                .pauseFor(1500)
+                                .start();
+                        }}
+                        options={{
+                            autoStart: true,
+                            loop: false,
+                            delay: 75,
+                            deleteSpeed: 50,
+                            cursor: "|",
+                        }}
+                    />
+                </div>
             </div>
         </section>
     );
