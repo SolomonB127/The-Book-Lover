032063265f426c0e8ecdf06e5c7d39689c5a29d2
SolomonB127 <solomonakinsulire@gmail.com>
2024-10-10 17:36:42 +0100

    Rendered in Recommendations components into file

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../HomeComponents/Hero";
+import Recommendations from "../HomeComponents/Recommendations";
 
 const Home = () => {
     return (
         <>
             <Hero />
+            <Recommendations />
         </>
     );
 };
