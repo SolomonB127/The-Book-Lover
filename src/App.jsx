9518f41ef93c9ceb438d503b025180691066e065
SolomonB127 <solomonakinsulire@gmail.com>
2024-10-10 01:22:08 +0100

    Set up routing with lazy loading and Suspense
    - Implemented createBrowserRouter for routing.
    - Added Layout component to wrap routes.
    - Integrated Loader component as a fallback for lazy loading.
    - Created Home page as the default route.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import Navbar from "./Components/Navbar";
+import React, { Suspense } from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { lazy } from "react";
+
+const Layout = lazy(() => import("/src/Components/Layout.jsx"))
+
+import Home from "./Components/Pages/Home";
 
 import "./App.css";
-import Home from "./Components/Home";
+import Loader from './Components/Loader';
+
+
 function App() {
+  const routes = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: <Home />
+        }
+      ]
+    }
+  ])
   return (
     <>
-      <Navbar />
-      <Home />
+      <Suspense fallback={<Loader />}>
+        <RouterProvider router={routes} />
+      </Suspense>
     </>
   );
 }
