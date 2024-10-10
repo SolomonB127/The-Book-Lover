c45cd5097ae6c72e2a6b8bd2ca607318b74d15c3
SolomonB127 <solomonakinsulire@gmail.com>
2024-10-10 17:34:55 +0100

    Modified styles

--- src/Components/HomeComponents/Hero.jsx
+++ src/Components/HomeComponents/Hero.jsx
@@ -3,7 +3,7 @@ import TypewriterComponent from "typewriter-effect";
 
 const Hero = () => {
     return (
-        <section className='flex items-center justify-start min-h-screen w-full p-4 mx-auto bg-[url("/src/assets/T.B.L_Cover.jpg")] bg-cover bg-center bg-no-repeat font-black text-black max-w-7xl'>
+        <section className='flex items-center justify-start min-h-screen w-full p-4 mx-auto bg-[url("/src/assets/T.B.L_Cover.jpg")] bg-cover bg-center bg-no-repeat font-black text-black max-w-7xl bg-yellow-300 font-serif'>
             <div className="text-center sm:text-left">
                 <div className="text-5xl sm:text-6xl md:text-7xl lg:text-6xl">
                     A Cozy Evening, <br />
